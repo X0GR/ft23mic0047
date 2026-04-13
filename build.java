68fbedfa9b3ff11835ec65f21bea5dc4145b17ff
X0GR <gnaneshwarofficial123@gmail.com>
2026-04-13 16:55:24 +0530

    Add build.java to display student information

---
+++ build.java
@@ -0,0 +1,14 @@
+package fat;
+
+public class fat23mic0047 {
+	public static void main(String[] args) {
+		 System.out.println("Student Name: Gnaneshwar Raja");
+		 System.out.println("Register Number: 23MIC0047");
+		 System.out.println("Course Name:Application Development & Deployment");
+		 System.out.println("Integerated Mtech computer science 5 YEAR course with colloboration with virtusa");
+		 System.out.println("Technical Stack: Jenkins & GitHub\n"
+		 		+ "Demonstrate Continuous Integration for a Library Management Application");
+		 System.out.println("13-04-26");
+		 System.out.println("Library Build sucessful");
+}
+}
